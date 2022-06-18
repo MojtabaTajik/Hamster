64233e27d4c5c58c746f96dff78fc35d10e4f10f
Mojtaba Tajik <onyfel@gmail.com>
2022-06-18 13:22:48 +0430

    Add configuration model which Hamster work based on them.

---
+++ source/hamster/Model/BackupOperation.cs
@@ -0,0 +1,9 @@
+namespace hamster.Model;
+
+public class BackupOperation
+{
+    public string Name { get; set; }
+    public string Command { get; set; }
+    public string BackupDir { get; set; }
+    public string RemoteDir { get; set; }
+}
