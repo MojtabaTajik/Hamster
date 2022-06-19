720ecee18ac10bb6438d808266b74894bad16232
Mojtaba Tajik <onyfel@gmail.com>
2022-06-19 12:09:06 +0430

    Add OperationExecuter.cs

---
+++ source/hamster/Services/OperationExecuter.cs
@@ -0,0 +1,18 @@
+using hamster.Model;
+
+namespace hamster.Services;
+
+public class OperationExecuter
+{
+    private readonly Config _config;
+
+    public OperationExecuter(Config config)
+    {
+        _config = config;
+    }
+
+    public void ExecuteOperation(string operationName)
+    {
+        
+    }
+}
