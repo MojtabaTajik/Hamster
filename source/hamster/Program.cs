e94fb48bdaf4d0dcb6e1a7afeb934c30c727a81e
Mojtaba Tajik <onyfel@gmail.com>
2022-06-19 12:09:59 +0430

    Initialize dependency injection and container.

--- source/hamster/Program.cs
+++ source/hamster/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using hamster.Model;
+using hamster.Services;
+
+const string configFileName = "config.json";
+if (!File.Exists(configFileName))
+    return;
+
+var configContent = File.ReadAllText(configFileName);
+var config = JsonSerializer.Deserialize<Config>(configContent);
+
+if (config == null || args.Length < 1)
+    return;
+
+var serviceProvider = new ServiceCollection()
+    .AddLogging(options =>
+    {
+        options.AddSimpleConsole(opt =>
+        {
+            opt.SingleLine = true;
+            opt.TimestampFormat = "MM/dd/yyyy hh:mm:ss => ";
+        });
+    })
+    .AddSingleton<OperationExecuter>()
+    .AddSingleton(config)
+    .BuildServiceProvider();
+
+var logger = serviceProvider.GetService<ILogger<Program>>();
+logger?.LogInformation("Hamster started");
+
+var operationName = args[0];
+var opExecuter = serviceProvider.GetService<OperationExecuter>();
+opExecuter?.ExecuteOperation(operationName);
+
+Console.Read();
