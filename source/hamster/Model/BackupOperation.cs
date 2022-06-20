c7c9b1687071b607eae26fd06f1d6cf60505b1e0
Mojtaba Tajik <onyfel@gmail.com>
2022-06-20 17:28:02 +0430

    Add Bucket field and let user configure the bucket to store data.

--- source/hamster/Model/BackupOperation.cs
+++ source/hamster/Model/BackupOperation.cs
@@ -2,6 +2,7 @@ namespace hamster.Model;
 
 public class BackupOperation
 {
+    public string Bucket { get; set; }
     public string Name { get; set; }
     public string Command { get; set; }
     public string BackupDir { get; set; }
