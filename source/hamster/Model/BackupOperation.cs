44f8f8f06529637ce8ec6bd5c16c55f3b6a9bbaa
Mojtaba Tajik <onyfel@gmail.com>
2022-06-20 19:00:22 +0430

    Refactor for better field naming

--- source/hamster/Model/BackupOperation.cs
+++ source/hamster/Model/BackupOperation.cs
@@ -7,7 +7,7 @@ public class BackupOperation
     public string Bucket { get; set; }
     public string Name { get; set; }
     public string Command { get; set; }
-    public string BackupDir { get; set; }
+    public string DirToBackup { get; set; }
 
     public string RemoteFileName
     {
