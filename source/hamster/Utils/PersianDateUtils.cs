ded028a5f790ec2731540b60e9c4cd92150a2c21
Mojtaba Tajik <onyfel@gmail.com>
2022-06-20 19:25:15 +0430

    include time of day

--- source/hamster/Utils/PersianDateUtils.cs
+++ source/hamster/Utils/PersianDateUtils.cs
@@ -9,6 +9,7 @@ public class PersianDateUtils
         DateTime now = DateTime.Now;
         PersianCalendar pc = new PersianCalendar();
         
-        return $"{pc.GetYear(now)}.{pc.GetMonth(now)}.{pc.GetDayOfMonth(now)}";
+        return $"{pc.GetYear(now)}.{pc.GetMonth(now)}.{pc.GetDayOfMonth(now)}" +
+               $"-{pc.GetHour(now)}.{pc.GetMinute(now)}.{pc.GetSecond(now)}";
     }
 }
