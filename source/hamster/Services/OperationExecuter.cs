dbf104f5c59c951f20913546586f08828521e2d3
Mojtaba Tajik <onyfel@gmail.com>
2022-06-20 16:01:09 +0430

    Execute command using bash and capture result

--- source/hamster/Services/OperationExecuter.cs
+++ source/hamster/Services/OperationExecuter.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using hamster.Model;
 
 namespace hamster.Services;
@@ -11,8 +12,33 @@ public class OperationExecuter
         _config = config;
     }
 
-    public void ExecuteOperation(string operationName)
+    public string ExecuteOperation(string operationName)
     {
-        
+        try
+        {
+            var operation = _config.Operations.FirstOrDefault(f => 
+                f.Name.ToLower().Equals(operationName.ToLower()));
+            
+            // Execute task
+            string result = string.Empty;
+            using var proc = new Process();
+            proc.StartInfo.FileName = "/bin/bash";
+            proc.StartInfo.Arguments = "-c \"" + operation!.Command + "\"";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.Start();
+
+            result += proc.StandardOutput.ReadToEnd();
+            result += proc.StandardError.ReadToEnd();
+
+            proc.WaitForExit();
+
+            return result;
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
     }
 }
