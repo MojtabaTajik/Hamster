21cc2026489d42d314cc81f332f31da246cbe8f4
Mojtaba Tajik <onyfel@gmail.com>
2022-06-20 16:39:37 +0430

    Add BucketExists method to check existence of bucket.

--- source/hamster/Services/ArvanObjectStorage.cs
+++ source/hamster/Services/ArvanObjectStorage.cs
@@ -1,4 +1,6 @@
 using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Util;
 using hamster.Model;
 
 namespace hamster.Services;
@@ -15,5 +17,10 @@ public class ArvanObjectStorage
         var awsCredentials = new Amazon.Runtime.BasicAWSCredentials(_config.AccessKey, _config.SecretKey);
         var s3Config = new AmazonS3Config { ServiceURL = _config.EndpointURL };
         _s3Client = new AmazonS3Client(awsCredentials, s3Config);
+    }
+
+    public async Task<bool> BucketExists(string bucketName)
+    {
+        return await AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
     }
 }
