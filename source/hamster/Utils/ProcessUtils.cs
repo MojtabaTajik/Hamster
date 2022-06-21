5661974e08107f9e8cec2c70faef39ac872c5d7a
Mojtaba Tajik <onyfel@gmail.com>
2022-06-21 13:25:20 +0430

    Automatically get the command processor based on running OS

--- source/hamster/Utils/ProcessUtils.cs
+++ source/hamster/Utils/ProcessUtils.cs
@@ -4,10 +4,10 @@ namespace hamster.Utils;
 
 public class ProcessUtils
 {
-    public static async Task<string> ExecuteProcess(string procName, string command)
+    public static async Task<string> ExecuteProcess(string command)
     {
         using var proc = new Process();
-        proc.StartInfo.FileName = procName;
+        proc.StartInfo.FileName = GetOsCommandEngine();
         proc.StartInfo.Arguments = "-c \"" + command + "\"";
         proc.StartInfo.UseShellExecute = false;
         proc.StartInfo.RedirectStandardOutput = true;
@@ -21,5 +21,19 @@ public class ProcessUtils
         await proc.WaitForExitAsync();
 
         return result;
+    }
+
+    private static string GetOsCommandEngine()
+    {
+        var os = Environment.OSVersion.Platform;
+
+        switch (os)
+        {
+            case PlatformID.Win32NT:
+                return "cmd.exe";
+            
+            default:
+                return "/bin/bash";
+        }
     }
 }
