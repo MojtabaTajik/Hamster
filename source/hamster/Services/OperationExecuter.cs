0c0a68c96a55571a1497b890c7ff3898352e8421
Mojtaba Tajik <onyfel@gmail.com>
2022-06-22 15:45:56 +0430

    Use CompressUtils to compress and send each part of compressed files to S3 bucket.

--- source/hamster/Services/OperationExecuter.cs
+++ source/hamster/Services/OperationExecuter.cs
@@ -9,13 +9,15 @@ public class OperationExecuter
 {
     private readonly Config _config;
     private readonly UploadFileUtils _uploadFileUtils;
+    private readonly CompressUtils _compressUtils;
     private readonly ILogger<OperationExecuter> _logger;
 
-    public OperationExecuter(Config config, ILogger<OperationExecuter> logger, UploadFileUtils uploadFileUtils)
+    public OperationExecuter(Config config, ILogger<OperationExecuter> logger, UploadFileUtils uploadFileUtils, CompressUtils compressUtils)
     {
         _config = config;
         _logger = logger;
         _uploadFileUtils = uploadFileUtils;
+        _compressUtils = compressUtils;
     }
 
     public async Task<bool> ExecuteOperation(string operationName)
@@ -51,26 +53,43 @@ public class OperationExecuter
 
             string bucketName = $"{operation.Name}-Backup".ToLower();
             
-            // Compress backup directory and upload it to bucket
-            var tempZipFilePath = Path.Combine(Path.GetTempPath(), operation.RemoteFileName);
-            if (File.Exists(tempZipFilePath))
-                File.Delete(tempZipFilePath);
+            // Compress backup directory
+            int maxAmazonS3BucketObjectSize = unchecked(5 * 1024 * 1024 * 1024);
 
-            ZipFile.CreateFromDirectory(backupDir, tempZipFilePath);
+            string zipFilePartsStoragePath = Path.Combine(Path.GetTempPath(), operation.Name);
+            var compressedFiles = _compressUtils.CompressDirectoryInParts(backupDir, zipFilePartsStoragePath,
+                operation.RemoteFileName, maxAmazonS3BucketObjectSize);
 
-            bool uploadResult = await _uploadFileUtils.UploadFile(bucketName, operation.RemoteFileName, tempZipFilePath);
-            _logger.LogInformation(uploadResult ? "Upload file success" : "Upload file failed");
-            
+            // Upload zip files (parts)
+            int partCounter = 0;
+            foreach (var compressedFile in compressedFiles)
+            {   
+                partCounter++;
+                string fileName = Path.GetFileName(compressedFile);
+                
+                _logger.LogInformation("Uploading [{PartCounter}/{CompressedFileLength}] => {FileName}",
+                    partCounter, compressedFile.Length, fileName);
+                
+                bool uploadResult = await _uploadFileUtils.UploadFile(bucketName, fileName, compressedFile);
+
+                if (!uploadResult)
+                {
+                    _logger.LogInformation("Upload [{FileName}] failed", fileName);
+                }
+            }
+
             // Clean up files
-            _logger.LogInformation("Cleanup files");
-            File.Delete(tempZipFilePath);
+            _logger.LogInformation("Cleanup up backup dir ...");
             new DirectoryInfo(backupDir).Delete(true);
+            
+            _logger.LogInformation("Cleanup up temp zip parts ...");
+            new DirectoryInfo(zipFilePartsStoragePath).Delete(true);
             
             return true;
         }
         catch (Exception e)
         {
-            _logger.LogError(e.Message);
+            _logger.LogError("Global exception => {EMessage}", e.Message);
             return false;
         }
     }
