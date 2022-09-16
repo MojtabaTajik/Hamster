2236daa2f14578ddbdd218644dc88f4934a9d0ac
Mojtaba <onyfel@gmail.com>
2022-09-16 12:34:28 +0430

    - Throw exception if can't get executing path

--- source/hamster/Utils/PathUtils.cs
+++ source/hamster/Utils/PathUtils.cs
@@ -1,11 +1,11 @@
 using System.Diagnostics;
-using System.Reflection;
 
 namespace hamster.Utils;
 
 public class PathUtils
 {
-    public static string ExecutingDirectory() => Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+    public static string ExecutingDirectory() => Path.GetDirectoryName(Process.GetCurrentProcess()?.MainModule?.FileName) 
+                                                 ?? throw new DirectoryNotFoundException();
 
     public static string BackupDir(string operationName) => Path.Combine(ExecutingDirectory(), operationName);
 
